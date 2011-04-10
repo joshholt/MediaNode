a306b24e770288efd5a9bf22e2adf6d998dde89a
Josh Holt <holt.josh@gmail.com>
2011-04-10 02:24:12 -0400

    Added logging to the server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 /*globals __dirname*/
 require('http').createServer(require('stack/stack')(
+	require('middleware/log')(),
   require('middleware/auth')(/\//, {'joshholt':'8j0GqIitKNzP7Ve9MenjPSx2GVI='}),
   require('middleware/listing')("/f/", __dirname+"/public/media/Music"),
   require('middleware/static')('/', __dirname + "/public", "index.html")
